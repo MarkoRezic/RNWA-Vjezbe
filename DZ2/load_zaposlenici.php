51a7f9d2cccb8d2f67576b8da0458d9209592dbb
Marko Rezić <markorezic1999@gmail.com>
2022-04-11 15:21:55 +0200

    dovrsen search, dodan css

--- DZ2/load_zaposlenici.php
+++ DZ2/load_zaposlenici.php
@@ -5,38 +5,38 @@ $db = mysqli_connect("localhost:3306","root","","hr");
 
 if($db) {
 
-$result2 = mysqli_select_db($db, "hr") or die("Doslo je do problema prilikom odabira baze...");
-$sql2="SELECT * FROM employees";
+    $result2 = mysqli_select_db($db, "hr") or die("Doslo je do problema prilikom odabira baze...");
+    $sql2="SELECT * FROM employees";
 
-$result2 = mysqli_query($db, $sql2) or die("Doslo je do problema prilikom izvrsavanja upita...");
-$n = mysqli_num_rows($result2);
-echo "<table border='1'>
-<tr>
-<th>First name</th>
-<th>Last name</th>
-<th>Actions</th>
-</tr>";
-if ($n > 0){
-	while ($myrow = mysqli_fetch_row($result2)){
-            echo "<tr>";
-            echo "<td>" . $myrow[1] . "</td>";
-            echo "<td>" . $myrow[2] . "</td>";
-            echo "<td>
-                <a href='#details'>DETAILS</a>
-                <a href='#update'>UPDATE</a>
-                <a href='#delete'>DELETE</a>
-            </td>";
-            echo "</tr>";
-            
-		}
-        echo "</table>";
-	}
-else {
-//echo "No patern rows returned<br>";
-}	
+    $result2 = mysqli_query($db, $sql2) or die("Doslo je do problema prilikom izvrsavanja upita...");
+    $n = mysqli_num_rows($result2);
+    echo "<table class='data-table'>
+    <tr>
+    <th>First name</th>
+    <th>Last name</th>
+    <th>Actions</th>
+    </tr>";
+    if ($n > 0){
+        while ($myrow = mysqli_fetch_row($result2)){
+                echo "<tr>";
+                echo "<td>" . $myrow[1] . "</td>";
+                echo "<td>" . $myrow[2] . "</td>";
+                echo "<td>
+                    <a href='#details'>DETAILS</a>
+                    <a href='#update'>UPDATE</a>
+                    <a href='#delete'>DELETE</a>
+                </td>";
+                echo "</tr>";
+                
+            }
+        }
+    else {
+        echo "<tr><td colspan='3'>Nema rezultata</td></tr>";
+    }	
+    echo "</table>";
 }
 else {
-echo "<br>Nije proslo spajanje<br>";
+    echo "<br>Nije proslo spajanje<br>";
 }
 
 ?>
--- DZ2/search_zaposlenici.php
+++ DZ2/search_zaposlenici.php
@@ -2,52 +2,49 @@
 //postaviti u xampp htdocs folder
 header("Access-Control-Allow-Origin: *");
 $s = $_REQUEST["s"];
-$hint = "";
 if ($s !== "") {
     $s = strtolower($s);
     $len=strlen($s);
+}
 
 $db = mysqli_connect("localhost:3306","root","","hr");
 
 if($db) {
 
-$result = mysqli_select_db($db, "hr") or die("Doslo je do problema prilikom odabira baze...");
-$sql="SELECT * FROM employees where first_name LIKE '%$s%' OR last_name LIKE '%$s%'";
+    $result = mysqli_select_db($db, "hr") or die("Doslo je do problema prilikom odabira baze...");
+    $sql="SELECT * FROM employees where first_name LIKE '%$s%' OR last_name LIKE '%$s%'";
 
-$result = mysqli_query($db, $sql) or die("Doslo je do problema prilikom izvrsavanja upita...");
+    $result = mysqli_query($db, $sql) or die("Doslo je do problema prilikom izvrsavanja upita...");
 
-echo "<table>
-      <tr>
-      <th>First name</th>
-      <th>Last name</th>
-      <th>Actions</th>
-      </tr>";
-$n=mysqli_num_rows($result);
-if ($n > 0){
-	while ($myrow=mysqli_fetch_row($result)){
-            echo "<tr>";
-            echo "<td>" . $myrow[1] . "</td>";
-            echo "<td>" . $myrow[2] . "</td>";
-            echo "<td>
-                <a href='#details'>DETAILS</a>
-                <a href='#update'>UPDATE</a>
-                <a href='#delete'>DELETE</a>
-            </td>";
+    echo "<table class='data-table'>
+        <tr>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Actions</th>
+        </tr>";
+    $n=mysqli_num_rows($result);
+    if ($n > 0){
+        while ($myrow=mysqli_fetch_row($result)){
+                echo "<tr>";
+                echo "<td>" . $myrow[1] . "</td>";
+                echo "<td>" . $myrow[2] . "</td>";
+                echo "<td>
+                    <a href='#details'>DETAILS</a>
+                    <a href='#update'>UPDATE</a>
+                    <a href='#delete'>DELETE</a>
+                </td>";
 
-            echo "</tr>";
-            
-		}
-        echo "</table>";
-	}
-else {
-//echo "<span>not found</span>";
-}	
+                echo "</tr>";
+                
+            }
+        }
+    else {
+        echo "<tr><td colspan='3'>Nema rezultata</td></tr>";
+    }	
+    echo "</table>";
 }
 else {
-echo "<br>Nije proslo spajanje<br>";
+    echo "<br>Nije proslo spajanje<br>";
 }
-	
-}
-
 
 ?>
